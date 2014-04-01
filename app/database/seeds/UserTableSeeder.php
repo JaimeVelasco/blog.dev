7b90ef5d7a4cd185047ac50e9745b32adb64d6ea
JaimeVelasco <jaime@photojv.com>
2014-04-01 09:14:58 -0500

    Changed username and password

--- app/database/seeds/UserTableSeeder.php
+++ app/database/seeds/UserTableSeeder.php
@@ -6,8 +6,8 @@ class UserTableSeeder extends Seeder {
 		DB::table('users')->delete();
 
 		$user = new User();
-		$user->email ='Jaime@photojv.com';
-		$user->password = Hash::make('letmeinnow');
+		$user->email ='Admin';
+		$user->password = Hash::make('admin123');
 		$user->save();
 	}
 }
