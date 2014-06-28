5da0b661e13c2aff7c4e779413e6e0d3b0310e13
JaimeVelasco <jaime@photojv.com>
2014-06-28 10:16:15 -0500

    ?

--- app/views/portfolio.blade.php
+++ app/views/portfolio.blade.php
@@ -22,6 +22,69 @@
             <div class="panel panel-default">
                 <div class="panel-heading">
                     <h4 class="text-center">
+                      City View Properties 
+                    </h4>
+                </div>
+                <div class="panel-body text-center">
+                    <p class="lead">
+                        <strong>Front-end Web Development</strong></p>
+                </div>
+                <ul class="list-group list-group-flush text-center">
+                    <li class="list-group-item"><i class="icon-ok text-danger"></i>Web portal for property management company</li>
+                    <li class="list-group-item"><i class="icon-ok text-danger"></i>May 2014</li>
+                    <li class="list-group-item"><i class="icon-ok text-danger"></i>JavaScript, jQuery<br>
+                    HTML5/CSS3, PyroCMS, GitHub</li>
+                <div class="panel-footer">
+                    <a class="btn btn-lg btn-block btn-default" href="https://github.com/JaimeVelasco"><i class="fa fa-github"></i> Visit Website</a>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-4">
+            <div class="panel panel-info">
+                <div class="panel-heading">
+                    <h4 class="text-center">
+                      WEB 
+                   </h4>
+                </div>
+                <div class="panel-body text-center">
+                    <p class="lead">
+                        <strong>Money Carousel</strong></p>
+                </div>
+                <ul class="list-group list-group-flush text-center">
+                    <li class="list-group-item"><i class="icon-ok text-danger"></i>Capstone Project</li>
+                    <li class="list-group-item"><i class="icon-ok text-danger"></i>Laravel, Bootstrap, jQuery, Github</li>
+                    <li class="list-group-item"><i class="icon-ok text-danger"></i>Developed in under 12 days with a team of 3 developers</li>
+                </ul>
+                <div class="panel-footer">
+                    <a class="btn btn-lg btn-block btn-info" href="http://moneycarousel.us">moneycarousel.us</a>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-4">
+            <div class="panel panel-default">
+                <div class="panel-heading">
+                    <h4 class="text-center">
+                       PHOTOGRAPHY 
+                    </h4>
+                </div>
+                <div class="panel-body text-center">
+                    <p class="lead">
+                        <strong>Portrait Scene</strong></p>
+                </div>
+                <ul class="list-group list-group-flush text-center">
+                    <li class="list-group-item"><i class="icon-ok text-danger"></i>+100 sessions last year</li>
+                    <li class="list-group-item"><i class="icon-ok text-danger"></i>+10 years experience</li>
+                    <li class="list-group-item"><i class="icon-ok text-danger"></i>Interested in old cameras and 19th century techniques</li>
+                </ul>
+                <div class="panel-footer">
+                    <a class="btn btn-lg btn-block btn-default" href="http://www.photojv.com">photojv.com</a>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-4">
+            <div class="panel panel-default">
+                <div class="panel-heading">
+                    <h4 class="text-center">
                       Github Repository 
                     </h4>
                 </div>
@@ -75,13 +138,13 @@
                 <ul class="list-group list-group-flush text-center">
                     <li class="list-group-item"><i class="icon-ok text-danger"></i>+100 sessions last year</li>
                     <li class="list-group-item"><i class="icon-ok text-danger"></i>+10 years experience</li>
-                    <li class="list-group-item"><i class="icon-ok text-danger"></i>Interested in old cameras and 19 century techniques</li>
+                    <li class="list-group-item"><i class="icon-ok text-danger"></i>Interested in old cameras and 19th century techniques</li>
                 </ul>
                 <div class="panel-footer">
                     <a class="btn btn-lg btn-block btn-default" href="http://www.photojv.com">photojv.com</a>
                 </div>
             </div>
-        </div>
+        </div
     </div>
 </div>
 
